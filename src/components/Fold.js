885c75e89024593e3444a3b038ae641c2f22b4b9
Izyk <lance_hemphill1234@yahoo.com>
2024-05-24 20:46:29 -0400

    redid fold section, added different styles, added selfie image

--- src/components/Fold.js
+++ src/components/Fold.js
@@ -1,6 +1,7 @@
-import React, {useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import { motion, transform } from 'framer-motion';
 
 import Selfie from '../assets/selfie.png'
 
@@ -9,11 +10,21 @@ import styles from '../styles/Fold.module.css';
 
 const Fold = () => {
 
+    const [hover, setHover] = useState(false)
+
+    const [clicked, setClicked] = useState(false)
+
     useEffect(() => {
         Aos.init({
             duration: 500
         })
     })
+
+    const hoverVariants = {
+        onHover: { backgroundColor: 'blue', width: '20rem', transition: { duration: 0.2, delay: 0 }},
+        offHover: { backgroundColor: 'black', width: '14rem', transition: { duration: 0.2, delay: 0 }}
+    }
+
 
     return (
         <section className={styles.fold_section}>
@@ -22,10 +33,20 @@ const Fold = () => {
                 <p>Front <br /> End <br /> Developer</p>
             </div>
 
-            {/* <div className={styles.text_container}>
-                <h1>Hello, I'm Lance, <br /> a Front-End Developer.</h1>
-                <img src={Selfie} alt='Lance Hemphill'/>
-            </div> */}
+            <div className={styles.content_container}>
+
+                <div data-aos="fade-right" className={styles.content_text}>
+                    <h1>Hello, I'm Lance, <br /> a front-end developer.</h1>
+                    <motion.button
+                    variants={hoverVariants}
+                    animate={hover ? "onHover" : "offHover"}
+                    onMouseOver={() => setHover(!hover)}
+                    onMouseLeave={() => setHover(!hover)}
+                    >Get Started</motion.button>
+                </div>
+
+                <img data-aos="fade-left" src={Selfie} alt='Lance Hemphill'/>
+            </div>
 
 
         </section>
