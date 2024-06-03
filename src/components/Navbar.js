5660cc45c4c3932b028638d247bb9d660e40c0e3
Izyk <lance_hemphill1234@yahoo.com>
2024-06-03 06:39:44 -0400

    removed 'contact me' from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,7 +44,6 @@ const Navbar = ({ closeNav }) => {
             <motion.nav variants={variants} animate={ closed ? "closed" : "open" } className={styles.nav}>
 
                 <div className={closed ? styles.close_list : styles.navbar}>
-                    <a href='/' onClick={() => toggle(!closed)}>Contact Me</a>
                     <a rel="noopener noreferrer" target='_blank' href='/Resume.pdf' aria-label='Resume' onClick={() => toggle(!closed)}>Resume</a>
                     <a rel="noopener noreferrer" target='_blank' href='https://www.linkedin.com/in/lance-hemphill-250b85218/' aria-label='LinkedIn' onClick={() => toggle(!closed)}>LinkedIn</a>
                     <a rel='noopener noreferrer' target='_blank' href='https://github.com/IzykGit' aria-label='Github'>Github</a>
