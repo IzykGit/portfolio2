2952d2c55bc1390467439d81fbed929613cfabc5
Lance <lance_hemphill1234@yahoo.com>
2024-06-21 13:55:44 -0400

    removed hire me button

--- src/components/Fold.js
+++ src/components/Fold.js
@@ -56,13 +56,6 @@ const Fold = () => {
                 <div data-aos="fade-right" className={styles.content_text}>
                     <h1>Hello, I'm Lance, <br /> a front-end developer.</h1>
                     <p>{displayedText}</p>
-                    <motion.a
-                    href='https://www.linkedin.com/in/lance-hemphill-250b85218/' target='_blank'
-                    variants={hoverVariants}
-                    animate={hover ? "onHover" : "offHover"}
-                    onMouseOver={() => setHover(!hover)}
-                    onMouseLeave={() => setHover(!hover)}
-                    >{'<hire_me />'}</motion.a>
                 </div>
 
                 <img data-aos="fade-left" data-aos-delay={100} src={Selfie} alt='Lance Hemphill'/>
