a83045decdcef1459c6076881a74a3f3853f390e
Lance Hemphill <lance_hemphill1234@yahoo.com>
2024-09-19 13:55:04 -0400

    updated resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ const Navbar = ({ closeNav }) => {
             <motion.nav variants={navVariants} animate={ closed ? "closed" : "open" } className={styles.nav}>
 
                 <div className={closed ? styles.close_list : styles.navbar}>
-                    <a rel="noopener noreferrer" target='_blank' href='/Resume.pdf' aria-label='Resume' onClick={() => toggle(!closed)}>Resume</a>
+                    <a rel="noopener noreferrer" target='_blank' href='/Lance Hemphill.pdf' aria-label='Resume' onClick={() => toggle(!closed)}>Resume</a>
                     <a rel="noopener noreferrer" target='_blank' href='https://www.linkedin.com/in/lance-hemphill-250b85218/' aria-label='LinkedIn' onClick={() => toggle(!closed)}>LinkedIn</a>
                     <a rel='noopener noreferrer' target='_blank' href='https://github.com/IzykGit' aria-label='Github'>Github</a>
                 </div>
